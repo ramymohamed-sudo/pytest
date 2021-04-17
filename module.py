3cc9c9e8391e7600ea3ca40c4b893afdd99a2299
Ramy Mohamed <ramy.mohamed@ibm.com>
2021-04-17 17:58:35 +0100

    commented mqtt methods

--- module.py
+++ module.py
@@ -26,26 +26,26 @@ def on_message(client, userdata, msg):
     print("** "+msg.topic+" "+str(msg.payload))
 
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+# client = mqtt.Client()
+# client.on_connect = on_connect
+# client.on_message = on_message
 
-broker_address = '9.161.154.25'  # "mqtt.eclipse.org"
-client.connect(broker_address, 1883, 60)
-topic = "MQTTsecondcode"
+# broker_address = '9.161.154.25'  # "mqtt.eclipse.org"
+# client.connect(broker_address, 1883, 60)
+# topic = "MQTTsecondcode"
 
 
-with open('UC3.4_synthetic_kpis.csv', newline='') as csvfile:
-    kpi_data_reader = csv.reader(csvfile, delimiter=',')
-    next(kpi_data_reader)
-    for row in kpi_data_reader:
-        # current_time = datetime.datetime.strptime(row[1],'%Y-%m-%d %H:%M:%S')
-        # delta = current_time - previous_time
-        sleep(1)    # delta
-        # previous_time = current_time
-        client.publish(topic, ','.join(row))
-        # client.publish(topic,json.loads(str(row)))
-        print(topic, ','.join(row))
+# with open('UC3.4_synthetic_kpis.csv', newline='') as csvfile:
+#     kpi_data_reader = csv.reader(csvfile, delimiter=',')
+#     next(kpi_data_reader)
+#     for row in kpi_data_reader:
+#         # current_time = datetime.datetime.strptime(row[1],'%Y-%m-%d %H:%M:%S')
+#         # delta = current_time - previous_time
+#         sleep(1)    # delta
+#         # previous_time = current_time
+#         client.publish(topic, ','.join(row))
+#         # client.publish(topic,json.loads(str(row)))
+#         print(topic, ','.join(row))
 
 
 def total(a, b):
